2b5efb35a8ca27b8b28f4aec9203319fa9e974cd
Adi Gudi <agudi24@gmail.com>
2020-05-16 22:53:25 -0700

    t

--- blogs/textclassification/txtcls1/trainer/model.py
+++ blogs/textclassification/txtcls1/trainer/model.py
@@ -40,7 +40,7 @@ BATCH_SIZE = 32
 
 # describe your data
 TARGETS = ['1', '2']
-MAX_DOCUMENT_LENGTH = 583
+MAX_DOCUMENT_LENGTH = 58
 CSV_COLUMNS = ['Age', 'General', 'Total_Bilirubin', 'Direct_Bilirubin', 'Alkaline_Phosphotase', 'Alamine_Aminotransferase', 'Aspartate_Aminotransferase', 'Total_Protiens', 'Albumin,Albumin_and_Globulin_Ratio', 'Dataset']
 LABEL_COLUMN = 'Dataset'
 DEFAULTS = [['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null']]
